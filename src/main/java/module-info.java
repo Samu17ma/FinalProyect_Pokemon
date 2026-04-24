8fd41d369cf9473b9eb0431214fc99df20c824c5
Samuel <sammarala@alu.edu.gva.es>
2026-04-24 12:09:49 +0200

    Updated software to jdk9

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module dsy.pokemonfinalproyect {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens dsy.pokemonfinalproyect to javafx.fxml;
+    exports dsy.pokemonfinalproyect;
+}
