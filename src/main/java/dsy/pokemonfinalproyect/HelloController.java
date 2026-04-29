f51fd798b31b3c725531f2b0c76427fea7c2c1f4
Samuel <sammarala@alu.edu.gva.es>
2026-04-29 15:50:27 +0200

    Added first scene and login.
    Signed-off-by: Samuel <sammarala@alu.edu.gva.es>

--- src/main/java/dsy/pokemonfinalproyect/HelloController.java
+++ src/main/java/dsy/pokemonfinalproyect/HelloController.java
@@ -1,14 +1,58 @@
 package dsy.pokemonfinalproyect;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public Button btn_login;
+
+    @FXML
+    public Button btn_sinup;
+
+    private void changeScene(String fxmlFile, String title) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Scene newScene = new Scene(loader.load());
+
+            Stage stage = (Stage) btn_login.getScene().getWindow();
+
+            stage.setTitle(title);
+            stage.setScene(newScene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void goToStartScene(ActionEvent actionEvent) {
+        changeScene("hello-view.fxml", "Pokémon Proyect");
+    }
+
+    public void logIn(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("login-view.fxml"));
+            Scene scene = new Scene(loader.load());
+
+            Stage stage = (Stage) btn_login.getScene().getWindow();
+            stage.setTitle("Login - Pokémon Project");
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void signUp(ActionEvent actionEvent) {
     }
 }
