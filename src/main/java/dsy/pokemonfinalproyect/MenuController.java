8c514578e88b5b3097bcb042705b4fedb6cb379d
Samuel <sammarala@alu.edu.gva.es>
2026-05-08 11:28:43 +0200

    Added login with the file
    Signed-off-by: Samuel <sammarala@alu.edu.gva.es>

--- src/main/java/dsy/pokemonfinalproyect/MenuController.java
+++ src/main/java/dsy/pokemonfinalproyect/MenuController.java
@@ -1,4 +1,22 @@
 package dsy.pokemonfinalproyect;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class MenuController {
+    @FXML
+    public Button btn_Exit;
+
+    @FXML
+    private Label lbl_trainerId;
+
+
+    public void setDisplayName(String userName) {
+        lbl_trainerId.setText(userName);
+    }
+
+    public void exitButton() {
+    }
 }
