cd0271b5a252fa24b5bcdf0d96c3806b33eb9285
Samuel <sammarala@alu.edu.gva.es>
2026-05-27 19:07:21 +0200

    Added the last comment on Type.java

--- src/main/java/dsy/pokemonfinalproyect/shapes/Type.java
+++ src/main/java/dsy/pokemonfinalproyect/shapes/Type.java
@@ -5,19 +5,93 @@ package dsy.pokemonfinalproyect.shapes;
  */
 
 public enum Type {
+    /**
+     * Normal type
+     */
+
     NORMAL,
+
+    /**
+     * Fire type
+     */
+
     FIRE,
+
+    /**
+     * Water type
+     */
+
     WATER,
+
+    /**
+     * Grass type
+     */
+
     GRASS,
+
+    /**
+     * Electric type
+     */
+
     ELECTRIC,
+
+    /**
+     * Ice type
+     */
+
     ICE,
+
+    /**
+     * Fighting type
+     */
+
     FIGHTING,
+
+    /**
+     * Poison type
+     */
+
     POISON,
+
+    /**
+     * Ground type
+     */
+
     GROUND,
+
+    /**
+     * Flying type
+     */
+
     FLYING,
+
+    /**
+     * Psychic type
+     */
+
     PSYCHIC,
+
+    /**
+     * Bug type
+     */
+
     BUG,
+
+    /**
+     * Rock type
+     */
+
     ROCK,
+
+    /**
+     * Ghost type
+     */
+
     GHOST,
+
+    /**
+     * Dragon type
+     */
+
     DRAGON
 }
